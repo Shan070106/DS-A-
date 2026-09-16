09f3876b66fcc0f6bdf071773fec9f64a0615d6d
Shanmugam A <kit27.am51@gmail.com>
2026-09-16 15:57:52 +0530

    Time: 11 ms (65.51%) | Memory: 16.8 MB (36.44%) - LeetSync

--- CPtemplate/583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ CPtemplate/583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,21 +1,21 @@
 class Solution {
-    int lcs(string& s1, string& s2, int i, int j, vector<vector<int>>& dp){
-        if(i < 0 || j < 0) return 0;
-
-        if(dp[i][j] != -1)
-            return dp[i][j];
-
-        if(s1[i] == s2[j]){
-            return dp[i][j] = 1 + lcs(s1,s2,i-1,j-1,dp);
-        }    
-        return dp[i][j] = max(lcs(s1,s2,i-1,j,dp), lcs(s1,s2,i,j-1,dp));
-        
-    }
 
 public:
     int minDistance(string word1, string word2) {
         int n1 = word1.size(), n2 = word2.size();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        return (n1+n2)-2*lcs(word1, word2, n1-1, n2-1, dp); 
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1));
+        
+        for(int i=1;i<=n1;i++){
+            for(int j=1;j<=n2;j++){
+                if(word1[i-1] == word2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i][j-1], dp[i-1][j]);
+                }
+            }
+        }
+
+        return (n1+n2)-2*dp[n1][n2]; 
     }
 };
